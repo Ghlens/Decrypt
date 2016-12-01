a047c5f187f3ba311c3ce7770fcaf5e278391c3b
Geert Lens <ghlens@xs4all.nl>
2016-12-01 11:27:30 +0100

    Formatted textDecrypt

--- textDecrypt.py
+++ textDecrypt.py
@@ -2,16 +2,21 @@ import random
 import time
 import sys
 
-inputString = sys.argv[1]
-testString = list(inputString)
-outputString = [''] * len(inputString)
 
-while testString != outputString:
-    for i in range(len(testString)):
-        if outputString[i] != testString[i]:
-            outputString[i] = chr(random.randint(32, 126))
-    sys.stdout.write('\r' + ''.join(outputString))
-    time.sleep(0.01)
-    sys.stdout.flush()
+def main():
+    inputString = sys.argv[1]
+    testString = list(inputString)
+    outputString = [''] * len(inputString)
 
-sys.stdout.write('\n')
+    while testString != outputString:
+        for i in range(len(testString)):
+            if outputString[i] != testString[i]:
+                outputString[i] = chr(random.randint(32, 126))
+        sys.stdout.write('\r' + ''.join(outputString))
+        time.sleep(0.01)
+        sys.stdout.flush()
+
+    sys.stdout.write('\n')
+
+if __name__ == '__main__':
+    main()
